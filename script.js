cec930feb07e474e2fbb924e10480924919ed4cf
Fradoka <franklindkamela@gmail.com>
2025-03-17 23:10:03 +0000

    completed level 100

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+const episodeCardTemplate = document.getElementById("episode-card");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function makePageForEpisodes(episodeList) {  
+  const episodeCards = episodeList.map(createEpisodeCard)
+  rootElem.append(...episodeCards);
 }
 
+function createEpisodeCard({id, url, name, season, number, airdate, airtime, airstamp, runtime, image:{medium, original}, summary, _links:{self:{href}}}) {
+  const episodeCard = episodeCardTemplate.content.cloneNode(true);
+  episodeCard.querySelector("h3").textContent = name;
+  episodeCard.querySelector("h4").textContent = `S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`;
+  episodeCard.querySelector("img").src = medium;
+  episodeCard.querySelector("p").innerHTML = summary;
+
+  return episodeCard;
+}
 window.onload = setup;
+
+
