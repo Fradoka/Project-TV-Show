abeef4c0afd4dd9c27bd549f96ebd1492e94e843
Fradoka <franklindkamela@gmail.com>
2025-04-02 15:04:32 +0100

    completed level 300

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 //You can edit ALL of the code here
 const rootElem = document.getElementById("root");
 const episodeCardTemplate = document.getElementById("episode-card");
-const allEpisodes = getAllEpisodes();
-const state = { allEpisodes, searchTerm: "" };
-let filteredEpisodes = "";
+const input = document.getElementById("search");
+const optionSelector = document.getElementById("episode-select");
+const totalEpisodesListed = document.getElementById("showed-episodes");
+const buttonReset = document.getElementById("reset");
+const state = { allEpisodes: [], searchTerm: "" };
 
-function setup() {
-  showEpisode(allEpisodes);
-}
-
-function showEpisode(episodesToShow) {
-  rootElem.innerHTML = "";
-  const filteredEpisodes = state.allEpisodes.filter(function (episode) {
-    return (
-      episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()) ||
-      episode.summary.toLowerCase().includes(state.searchTerm.toLowerCase())
-    );
-  });
-
-  let episodesToRender;
-  if (Array.isArray(episodesToShow)) {
-    episodesToRender = filteredEpisodes;
-  } else {
-    episodesToRender = [episodesToShow];
+async function setup() {
+  try {
+    const response = await fetch ("https://api.tvmaze.com/shows/82/episodes");
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    state.allEpisodes = await response.json();
+    showEpisode(state.allEpisodes);
+  } catch (error) {
+      alert(`Error fetching data: ${error.message}`);
+      console.error('Error fetching data:', error);
   }
-
-  const episodeCards = episodesToRender.map(createEpisodeCard);
-  rootElem.append(...episodeCards);
-  document.getElementById("showed-episodes").innerText = `Showing ${filteredEpisodes.length}/ 73 Episodes.`;
 }
 
-function createEpisodeCard({name, season, number, image: { medium, original }, summary, _links: { self: { href } },}) {
+function createEpisodeCard({name, season, number, image: { medium, original }, summary, _links: { self: { href }, },}) {
   const episodeCard = episodeCardTemplate.content.cloneNode(true);
   episodeCard.querySelector("h3").textContent = name;
   episodeCard.querySelector("h4").textContent = `S${season
@@ -41,39 +30,57 @@ function createEpisodeCard({name, season, number, image: { medium, original }, s
   return episodeCard;
 }
 
-const input = document.querySelector("input");
-input.addEventListener("keyup", function () {
-  state.searchTerm = input.value;
-  showEpisode(state.allEpisodes);
-});
-
 function createOption(episode) {
-  const option = document.getElementById("option-template").content.cloneNode(true);
-  function makeEpisodeTitle() {
-    return `S${episode.season.toString().padStart(2, "0")}E${episode.number
-      .toString()
-      .padStart(2, "0")} - ${episode.name}`;
-  }
-  const title = makeEpisodeTitle();
-  option.querySelector("option").textContent = title;
-  option.querySelector("option").value = episode.id;
+  const option = document.createElement("option");
+  option.value = episode.id;
+  option.textContent = `S${String(episode.season).padStart(2, "0")}E${String(
+    episode.number
+  ).padStart(2, "0")} - ${episode.name}`;
   return option;
 }
-const selectElement = document.getElementById("episode-select");
-allEpisodes.forEach((episode) => {
-  const optionElement = createOption(episode);
-  selectElement.appendChild(optionElement);
-});
 
-function handleSelect(event) {
-  const chosenEpisode = allEpisodes.find((episode) => episode.id == this.value);
-  if (chosenEpisode) {
-    showEpisode(chosenEpisode);
-    document.getElementById("showed-episodes").innerText = `Showing 1/ 73 Episodes.`;
+function showEpisode(episodesToShow) {
+  rootElem.innerHTML = "";
+
+  let episodesToRender;
+  if (Array.isArray(episodesToShow)) {
+    episodesToRender = episodesToShow;
   } else {
-    showEpisode(allEpisodes);
+    episodesToRender = [episodesToShow];
   }
+
+  const episodeCards = episodesToRender.map(createEpisodeCard);
+  rootElem.append(...episodeCards);
+  const episodesList = episodesToRender.map(createOption);
+  optionSelector.innerHTML = `<option value="">-- Select an episode --</option>`;
+  optionSelector.append(...episodesList);
+  totalEpisodesListed.innerText = `Showing ${episodesToRender.length}/ ${state.allEpisodes.length} Episodes.`;
 }
 
-selectElement.addEventListener("change", handleSelect);
+input.addEventListener("input", function () {
+  state.searchTerm = input.value;
+  const filteredEpisodes = state.allEpisodes.filter((episode) =>
+      episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()) ||
+      episode.summary.toLowerCase().includes(state.searchTerm.toLowerCase())
+  );
+    showEpisode(filteredEpisodes);
+  return filteredEpisodes
+});
+
+optionSelector.addEventListener("change", function () {
+  const chosenEpisode = state.allEpisodes.find((episode) => episode.id == this.value);
+  if (chosenEpisode) {
+    rootElem.innerHTML = "";
+    showEpisode(chosenEpisode);
+  }
+  input.value = "";
+  state.searchTerm = "";
+});
+
+buttonReset.addEventListener("click", () => {
+  input.value = "";
+  state.searchTerm = "";
+  showEpisode(state.allEpisodes);
+})
+
 window.onload = setup;
